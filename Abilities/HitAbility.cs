2d11283d254d9598d31fad32273adf94ffadc929
Pyrolyzed <blevinse32@gmail.com>
2022-02-09 17:55:09 -0600

    Removed item in constructor of HitAbility

--- Abilities/HitAbility.cs
+++ Abilities/HitAbility.cs
@@ -7,25 +7,28 @@ namespace PyroAPI.Abilities
 {
     public class HitAbility : MonoBehaviour
     {
-        public Action<Creature, CollisionInstance> OnHit;
-        public Item item;
+        public Action<Creature, CollisionInstance> OnHit { get; set; }
+        public Item Item { get; set; }
         public HitCondition Condition { get; set; }
 
         /*
          * Create an ALWAYS ACTIVE hit ability
          */
-        public HitAbility Setup(Item _item, Action<Creature, CollisionInstance> onHit)
+        public HitAbility Setup(Action<Creature, CollisionInstance> onHit)
         {
-            Setup(_item, onHit, (creature, instance) => true);
+            Setup(onHit, (creature, instance) => true);
             return this;
         }
 
         /*
          * Create a hit ability that must satisfy the Condition
          */
-        public HitAbility Setup(Item _item, Action<Creature, CollisionInstance> onHit, Func<Creature, CollisionInstance, bool> condition)
+        public HitAbility Setup(Action<Creature, CollisionInstance> onHit,
+            Func<Creature, CollisionInstance, bool> condition)
         {
-            item = _item;
+            if (!gameObject.GetComponent<Item>())
+                throw new Exception("No Item on HitAbility gameObject");
+            Item = gameObject.GetComponent<Item>();
             OnHit = onHit;
 
             Condition = gameObject.AddComponent<HitCondition>().Setup(condition);
@@ -33,11 +36,12 @@ namespace PyroAPI.Abilities
             CollisionHandler.CollisionEvent onCollision = instance =>
             {
                 if (instance.damageStruct.hitRagdollPart?.ragdoll?.creature is Creature creature &&
-                    item.handlers.TrueForAll(hand => hand.creature.isPlayer) && Condition.TryCondition(creature, instance) &&
+                    Item.handlers.TrueForAll(hand => hand.creature.isPlayer) &&
+                    Condition.TryCondition(creature, instance) &&
                     Condition.Allowed)
                     OnHit?.Invoke(creature, instance);
             };
-            item.collisionHandlers.ForEach(i => i.OnCollisionStartEvent += onCollision);
+            Item.collisionHandlers.ForEach(i => i.OnCollisionStartEvent += onCollision);
 
             return this;
         }
